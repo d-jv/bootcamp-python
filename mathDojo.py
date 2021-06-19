8e0a13021a4a49a7dd472e806c3c23f6458dcf6b
diego-jv <diego.jeria@usach.cl>
2021-06-19 11:21:15 -0400

    mathDojo with Bonuses: standard deviation

--- mathDojo.py
+++ mathDojo.py
@@ -11,7 +11,6 @@ class MathDojo:
             num = int(num)
             map(int, nums)
         except ValueError: return None
-
         self.result += num
         for x in nums:
             self.result += x
@@ -24,18 +23,20 @@ class MathDojo:
         return self
     
     def standardDeviation (self, num, *nums):
-        squareDistance = 0
         numsList = [num]
         for i in nums:
             numsList.append(i)
         sumatoria = sum(numsList)
         avg = sumatoria / len(numsList)
-        #falta por terminar
-
+        sumDist = 0
+        for i in numsList:
+            sumDist += (i - avg)**2
+        stdDev = (sumDist / len(numsList))**0.5
+        self.result = stdDev
+        return self
 
 
 md = MathDojo()
-
 hehe = md.add(2).add(1,5,1).subtract(3,2).result
 print(hehe)
 
@@ -47,7 +48,13 @@ print(paProbarAdd)
 paProbar2 = MathDojo()
 # Escriba el método de subtract y pruébelo llamándolo 3 veces, con diferentes números de argumentos cada vez
 paProbarSubtract = paProbar2.add(10).subtract(1,1).subtract(3,1).subtract(5,4,3,2).result
-print(paProbarSubtract)	
+print(paProbarSubtract)
+
+# BONUS: Calcule la desviacion estandar (sin librerias)
+paProbar3 = MathDojo()
+paProbarStdDev = paProbar3.standardDeviation(6,2,3,1).result
+print(paProbarStdDev)
+
 
 # --------------------------- TESTING ---------------------------------------
 class MathDojoTesting(unittest.TestCase):
@@ -61,5 +68,5 @@ class MathDojoTesting(unittest.TestCase):
     def tearDown(self) -> None:
         return super().tearDown()
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
